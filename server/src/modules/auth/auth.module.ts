0625bcdac406492eb0f4de4054847133ec248dce
Minarulvortex <g57504444@gmail.com>
2025-06-27 06:14:05 +0600

    Create auth.module.ts

---
+++ server/src/modules/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService, AuthGuard],
+  exports: [AuthService, AuthGuard],
+})
+export class AuthModule {}
